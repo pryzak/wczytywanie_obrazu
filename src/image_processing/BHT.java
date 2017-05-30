4f84a04ad6b5eebfa3e851bc5f675b5e45858422
pryzak <pryziu@o2.pl>
2017-05-30 19:54:52 +0200

    jakies pierdoły i gitignore

--- src/image_processing/BHT.java
+++ src/image_processing/BHT.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package image_processing;
 
-/**
- *
- * @author Krzysiek
- */
 public class BHT {
     
 //    public double meanUntilL(int l, int[] histogram) {
--- src/image_processing/Filters.java
+++ src/image_processing/Filters.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package image_processing;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 
-/**
- *
- * @author Krzysiek
- */
 public class Filters {
 
     public static float lowPass[] = new float[]{1.f / 9, 1.f / 9, 1.f / 9, 1.f / 9, 1.f / 9, 1.f / 9, 1.f / 9, 1.f / 9, 1.f / 9};
