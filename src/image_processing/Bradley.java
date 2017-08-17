27bd50c12d4ca235bbc63dd924adbd5b40e2002b
pryzak <pryziu@o2.pl>
2017-08-17 16:21:06 +0200

    poprawka bradleya

--- src/image_processing/Bradley.java
+++ src/image_processing/Bradley.java
@@ -27,17 +27,38 @@ public class Bradley {
                 int y2 = j + s/2;
                 int count = (x2 - x1) * (y2 - y1);
                 int sum = 0;
-                if(x2 >= 0 && x2 < bi.getWidth() && y2 >= 0 && y2 < bi.getHeight())
-                    sum += intImg[x2][y2];
-                if(x2 >= 0 && x2 < bi.getWidth() && y1-1 >= 0 && y1-1 < bi.getHeight())
-                    sum -= intImg[x2][y1-1];
-                if(x1-1 >= 0 && x1-1 < bi.getWidth() && y2 >= 0 && y2 < bi.getHeight())
-                    sum -= intImg[x1-1][y2];
-                if(x1-1 >= 0 && x1-1 < bi.getWidth() && y1-1 >= 0 && y1-1 < bi.getHeight())
-                    sum += intImg[x1-1][y1-1];
-//                sum = intImg[x2][y2] - intImg[x2][y1-1] - intImg[x1-1][y2] + intImg[x1-1][y1-1];
+//                if(x2 >= 0 && x2 < bi.getWidth() && y2 >= 0 && y2 < bi.getHeight())
+//                    sum += intImg[x2][y2];
+//                if(x2 >= 0 && x2 < bi.getWidth() && y1-1 >= 0 && y1-1 < bi.getHeight())
+//                    sum -= intImg[x2][y1-1];
+//                if(x1-1 >= 0 && x1-1 < bi.getWidth() && y2 >= 0 && y2 < bi.getHeight())
+//                    sum -= intImg[x1-1][y2];
+//                if(x1-1 >= 0 && x1-1 < bi.getWidth() && y1-1 >= 0 && y1-1 < bi.getHeight())
+//                    sum += intImg[x1-1][y1-1];
+                if (x1 - 1 < 0)
+                    x1 = 1;
+                if(x2 < 0)
+                    x2 = 0;
+                if (x2 >= bi.getWidth())
+                    x2 = bi.getWidth() - 1;
+                if (x1 >= bi.getWidth())
+                    x1 = bi.getWidth() - 1;
+                if (y1 - 1 < 0)
+                    y1 = 1;
+                if (y2 < 0)
+                    y2 = 0;
+                if (y2 >= bi.getHeight())
+                    y2 = bi.getHeight() - 1;
+                if (y1 >= bi.getHeight())
+                    y1 = bi.getHeight() - 1;
+                sum = intImg[x2][y2] - intImg[x2][y1-1] - intImg[x1-1][y2] + intImg[x1-1][y1-1];
+//                try {
+//                    sum = intImg[x2][y2] - intImg[x2][y1-1] - intImg[x1-1][y2] + intImg[x1-1][y1-1];
+//                } catch (ArrayIndexOutOfBoundsException e) {
+//                    
+//                }
                 int gray = new Color(bi.getRGB(i, j)).getRed();
-                if((gray * count) <= (sum * (100 - t) / 100))
+                if((double)((double)gray * (double)count) <= (double) ((double)sum * ((double)100 - (double)t) / (double)100))
                     out[i][j] = 0;
                 else
                     out[i][j] = 255;
--- src/image_processing/KapurSahooWong.java
+++ src/image_processing/KapurSahooWong.java
@@ -61,4 +61,84 @@ public class KapurSahooWong {
         return threshold;
     }
      
+    public int getThreshold2(BufferedImage bi) {
+        int[] data = new HistogramHelper().getHistogram(new ImageToArray().convertToArrayGray(bi));
+        int threshold=-1;
+		int ih, it;
+		int first_bin;
+		int last_bin;
+		double tot_ent;  /* total entropy */
+		double max_ent;  /* max entropy */
+		double ent_back; /* entropy of the background pixels at a given threshold */
+		double ent_obj;  /* entropy of the object pixels at a given threshold */
+		double [] norm_histo = new double[256]; /* normalized histogram */
+		double [] P1 = new double[256]; /* cumulative normalized histogram */
+		double [] P2 = new double[256]; 
+
+		double total =0;
+		for (ih = 0; ih < 256; ih++ ) 
+			total+=data[ih];
+
+		for (ih = 0; ih < 256; ih++ )
+			norm_histo[ih] = data[ih]/total;
+
+		P1[0]=norm_histo[0];
+		P2[0]=1.0-P1[0];
+		for (ih = 1; ih < 256; ih++ ){
+			P1[ih]= P1[ih-1] + norm_histo[ih];
+			P2[ih]= 1.0 - P1[ih];
+		}
+
+		/* Determine the first non-zero bin */
+		first_bin=0;
+		for (ih = 0; ih < 256; ih++ ) {
+			if ( !(Math.abs(P1[ih])<2.220446049250313E-16)) {
+				first_bin = ih;
+				break;
+			}
+		}
+
+		/* Determine the last non-zero bin */
+		last_bin=255;
+		for (ih = 255; ih >= first_bin; ih-- ) {
+			if ( !(Math.abs(P2[ih])<2.220446049250313E-16)) {
+				last_bin = ih;
+				break;
+			}
+		}
+
+		// Calculate the total entropy each gray-level
+		// and find the threshold that maximizes it 
+		max_ent = Double.MIN_VALUE;
+
+		for ( it = first_bin; it <= last_bin; it++ ) {
+			/* Entropy of the background pixels */
+			ent_back = 0.0;
+			for ( ih = 0; ih <= it; ih++ )  {
+				if ( data[ih] !=0 ) {
+					ent_back -= ( norm_histo[ih] / P1[it] ) * Math.log ( norm_histo[ih] / P1[it] );
+				}
+			}
+
+			/* Entropy of the object pixels */
+			ent_obj = 0.0;
+			for ( ih = it + 1; ih < 256; ih++ ){
+				if (data[ih]!=0){
+				ent_obj -= ( norm_histo[ih] / P2[it] ) * Math.log ( norm_histo[ih] / P2[it] );
+				}
+			}
+
+			/* Total entropy */
+			tot_ent = ent_back + ent_obj;
+
+			// IJ.log(""+max_ent+"  "+tot_ent);
+			if ( max_ent < tot_ent ) {
+				max_ent = tot_ent;
+				threshold = it;
+			}
+		}
+		return threshold;
+    }
+
+     
 }
